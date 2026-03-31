ce8cf36f02634b7fa361e4f3d3b53eada18a3271
Octane <octaneluna@gmail.com>
2026-03-31 11:54:40 +0200

    Code refactoring

---
+++ data/coupons.js
@@ -0,0 +1,15 @@
+export const SITE_URL = 'https://coupon.prodigeek.it/';
+export const SITE_LASTMOD = '2026-03-26';
+
+export const coupons = {
+    esclusiviProdigeek: [],
+    altriEsclusiviProdigeek: [],
+    standardProdigeek: [],
+    generici: [
+        { discount: 40, min: 329, code: 'BDIT40', currency: '€' },
+        { discount: 25, min: 209, code: 'BDIT25', currency: '€' },
+        { discount: 12, min: 109, code: 'BDIT12', currency: '€' },
+        { discount: 8, min: 69, code: 'BDIT08', currency: '€' },
+        { discount: 4, min: 35, code: 'BDIT04', currency: '€' }
+    ]
+};
